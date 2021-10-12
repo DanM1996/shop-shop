eea4685d45f06701596f348c6693ddb12997d94b
Dan <bmdmartinez@gmail.com>
2021-10-12 11:57:51 -0400

    created and ran tests, created global state object to be used in all components

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,23 @@
+import { useProductReducer } from "./reducers";
+import React, { createContext, useContext } from 'react';
+
+const StoreContext = createContext();
+// Provider takes the data in the state and makes it available as a prop to all components
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+}
+
+export { StoreProvider, useStoreContext };
